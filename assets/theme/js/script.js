3c1f3956871e3470a7098af28befbcbf91a79747
amreshthakur1 <120268841+amreshthakur1@users.noreply.github.com>
2023-04-19 21:51:37 +0545

    Add files via upload

---
+++ assets/theme/js/script.js
@@ -0,0 +1,18 @@
+
+
+// scrolltotop
+
+window.onscroll = function() {
+	var e = document.getElementById("scrolltop");
+    if (!e) {
+    	e = document.createElement("a");
+        e.id = "scrolltop";
+        e.href = "#";
+        document.body.appendChild(e);
+	}
+	e.style.display = document.documentElement.scrollTop >300 ? "block" : "none";
+    e.onclick = (ev) => {
+    	ev.preventDefault();
+        document.documentElement.scrollTop = 0;
+    };
+};
