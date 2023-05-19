63902217ae45b559f393e74e12a299dfbcab5459
amreshthakur1 <120268841+amreshthakur1@users.noreply.github.com>
2023-05-19 05:52:24 +0545

    Add files via upload

--- assets/theme/js/script.js
+++ assets/theme/js/script.js
@@ -1,5 +1,4 @@
-// scrolltotop
-
+// -----------------------------------------scrolltotop ---------------------------------------//
 window.onscroll = function() {
 	var e = document.getElementById("scrolltop");
     if (!e) {
@@ -14,13 +13,28 @@ window.onscroll = function() {
         document.documentElement.scrollTop = 0;
     };
 };
+// -----------------------------------------scrolltotop ---------------------------------------//
 
-
+//----------------------------------Google tag (gtag.js)---------------------------------------------//
 {<script async src="https://www.googletagmanager.com/gtag/js?id=G-TN9TC8YPC6"></script>
 
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-TN9TC8YPC6');}
+  gtag('config', 'G-TN9TC8YPC6')};
+//----------------------------------Google tag (gtag.js)---------------------------------------------//
 
+
+
+//---------------------------------------Google Tag Manager---------------------------------------------------//
+  (function (w, d, s, l, i) {
+    w[l] = w[l] || []; w[l].push({
+        'gtm.start':
+            new Date().getTime(), event: 'gtm.js'
+    }); var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'GTM-WJNBHBX');
+
+//--------------------------------------End Google Tag Manager-----------------------------------//
